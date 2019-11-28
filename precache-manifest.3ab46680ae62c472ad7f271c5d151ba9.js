c47727ba53d8b4b16cc5ac202d7d245a00a1e574
codetologist <codetologist@gmail.com>
2019-11-28 11:32:18 -0400

    Updates

---
+++ precache-manifest.3ab46680ae62c472ad7f271c5d151ba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83d4a16923ed496e0f64c289e8d679f",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "b9357e5320bf441592b8",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "092150d62b1676e14ea6",
+    "url": "/robofriend/static/css/main.6ccc0d2a.chunk.css"
+  },
+  {
+    "revision": "b9357e5320bf441592b8",
+    "url": "/robofriend/static/js/2.27d4e179.chunk.js"
+  },
+  {
+    "revision": "092150d62b1676e14ea6",
+    "url": "/robofriend/static/js/main.5f94e473.chunk.js"
+  },
+  {
+    "revision": "fcd27cbb607344446732",
+    "url": "/robofriend/static/js/runtime-main.8e699037.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
